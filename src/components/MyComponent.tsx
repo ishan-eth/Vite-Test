1d373a9bfe1fc7d3055e2283b8dd53abe4e1a81f
Ishan Gupta <ishangupta3121@gmail.com>
2025-02-16 00:38:14 +0530

    new change

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-
 type Props = {
   title: string;
   count: number;
 };
 
 const MyComponent = ({ title, count }: Props) => {
-  return <div>{title}</div>;
+  return (
+    <div>
+      {title}
+      {count}
+    </div>
+  );
 };
 
 export default MyComponent;
